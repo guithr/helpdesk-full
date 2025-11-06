2227453541ef52e2940340e4c74167355b037c8e
guithr <guilherme.thrmatos@gmail.com>
2025-11-06 13:15:42 -0300

    feath(pages) adiciona pages NotFound e Unauthorized

---
+++ frontend/src/pages/Unauthorized/Unauthorized.tsx
@@ -0,0 +1,8 @@
+export default function Unauthorized() {
+  return (
+    <div className="flex flex-coL">
+      <h1>Acesso negado</h1>
+      <p>Você não tem permissão para acessar esta página.</p>
+    </div>
+  );
+}
